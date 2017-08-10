b6ae3215d06f8f658c23b8df8d0b6eff302d133c
Nicolas Singh <Nicolas_Singh@gap.com>
2017-08-10 18:23:32 -0400

    remove timezone variable and configure accesses for default rabbit user

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ else
   sleep 5
   rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS
   rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
+  rabbitmqctl set_permissions $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
 fi
 
 sleep infinity
